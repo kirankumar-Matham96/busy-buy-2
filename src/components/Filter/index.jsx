a6a9ec2b7377d603742b786f1e2bf562dcd729b4
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-08-14 07:47:33 +0530

    feat: added filter component to home

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,7 +1,35 @@
+import filterStyles from "./index.module.css";
+
 export const Filter = () => {
   return (
-    <div>
-        Filter
+    <div className={filterStyles.bgContainer}>
+      <h2>Filter</h2>
+      <div className={filterStyles.priceFilter}>
+        <h3>Price</h3>
+        <input type="range" name="price" id="price" min="50" max="10000" />
+      </div>
+
+      <div className={filterStyles.categoryFilter}>
+        <h3>Category</h3>
+        <div className={filterStyles.categories}>
+          <label htmlFOR="Mens">
+            <input type="checkbox" id="Mens" />
+            &nbsp; Mens Wear
+          </label>
+          <label htmlFOR="Womens">
+            <input type="checkbox" id="Womens" />
+            &nbsp; Womens Wear
+          </label>
+          <label htmlFOR="Electronincs">
+            <input type="checkbox" id="Electronincs" />
+            &nbsp; Electronics
+          </label>
+          <label htmlFOR="Jwelery">
+            <input type="checkbox" id="Jwelery" />
+            &nbsp; Jwelery
+          </label>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import { ItemsContainer } from "../../components/ItemsContainer";
+import { Filter } from "../../components/Filter";
+import homeStyles from "./index.module.css";
 
 export const Home = () => {
   return (
-    <div>
+    <div className={homeStyles.bgContainer}>
       <ItemsContainer />
+      <div className={homeStyles.filterContainer}>
+        <Filter />
+      </div>
     </div>
   );
 };
